014ffb0a8529418e3e6383b1e17aa7126bd7d7d7
Maurice Ford <mauriceford@gmail.com>
2025-03-11 23:31:08 +0000

    oops final commit

--- app.rb
+++ app.rb
@@ -1,9 +1,47 @@
-require "sinatra"
-require "sinatra/reloader"
+ require "sinatra"
+ require "sinatra/reloader"
+ require "dotenv/load"
+ require "http"
+ require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  apik = ENV.fetch("EXR")
+  baseurl = "https://api.exchangerate.host/list?access_key=#{apik}"
+
+  urlraw = HTTP.get(baseurl)
+  urljson = JSON.parse(urlraw)
+
+  r1 = urljson["currencies"]
+  @r2 = r1.keys
+
+  erb(:homepage)
+end
+
+get("/:ccode") do
+  @country_code = params.fetch("ccode")
+  apik = ENV.fetch("EXR")
+  baseurl = "https://api.exchangerate.host/list?access_key=#{apik}"
+
+  urlraw = HTTP.get(baseurl)
+  urljson = JSON.parse(urlraw)
+
+  r1 = urljson["currencies"]
+  @r2 = r1.keys
+
+  erb(:ccode)
+
+end
+
+get("/:ccode/:ccode_b") do
+  @country_code = params.fetch("ccode")
+  @country_code_b = params.fetch("ccode_b")
+  apik = ENV.fetch("EXR")
+  baseurl = "https://api.exchangerate.host/convert?from=#{@country_code}&to=#{@country_code_b}&amount=1&access_key=#{apik}"
+
+  urlraw = HTTP.get(baseurl)
+  urljson = JSON.parse(urlraw)
+
+  @exrate = urljson["result"]
+
+  erb(:conversion)
 end
